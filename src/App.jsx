2d8f991b8b7e82f93aa5f06e2cf8f5c50a826e75
George Fawcett <georgealanfawcett@gmail.com>
2017-04-04 19:21:10 +0000

    Adds componentDidMount function

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,8 @@ import MessageList from './MessageList.jsx';
 
 
 
-const messageData = {
-  currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
-  messages: [
-    {
-      username: "Bob",
-      content: "Has anyone seen my marbles?",
-    },
-    {
-      username: "Anonymous",
-      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-    }
-  ]
-}
+
+
 
 
 
@@ -32,10 +21,34 @@ class App extends Component {
 
  constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+       currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+  messages: [
+    {
+      username: "Bob",
+      content: "Has anyone seen my marbles?",
+    },
+    {
+      username: "Anonymous",
+      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+    }
+  ]
+    }
   }
 
-
+// in App.jsx
+componentDidMount() {
+  console.log("componentDidMount <App />");
+  setTimeout(() => {
+    console.log("Simulating incoming message");
+    // Add a new message to the list of messages in the data store
+    const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+    const messages = this.state.messages.concat(newMessage)
+    // Update the state of the app component.
+    // Calling setState will trigger a call to render() in App and all child components.
+    this.setState({messages: messages})
+  }, 3000);
+}
 
 
 
@@ -48,11 +61,11 @@ class App extends Component {
   <a href="/" className="navbar-brand">Chatty</a>
 </nav>
 
-<MessageList messages = {messageData.messages} />
+<MessageList messages = {this.state.messages} />
 
 
 
-<ChatBar currentUser={messageData.currentUser.name} />
+<ChatBar currentUser={this.state.currentUser.name} />
 </div>
     );
   }
